7fcfc8fba28b6139b97d5899e77e30f070f71331
isaac stott <isaac.cloyd@gmail.com>
2020-06-10 19:53:38 -0600

    met mvp

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,7 +1,6 @@
 const db = require("../data/connection.js");
 
 module.exports = {
-    all,
     add,
     addStep,
     update,
@@ -12,7 +11,7 @@ module.exports = {
 
 }
 
-function all() {
+function find() {
     return db("schemes");
 }
 
@@ -20,13 +19,36 @@ function add(scheme) {
     return db("schemes").insert(scheme, "id")
 }
 
-function update(id, changes) {
+function addStep(step, scheme_id) {
+    return db("steps")
+        .where(scheme_id)
+        .insert(step, "id")
+}
+
+function update(changes, id) {
     return db("schemes")
         .where({id})
-        .update({changes});
+        .update(changes, "id")
+        .then(() => {
+            return findById(id)
+        });
 }
 
 function findById(id) {
     return db("schemes")
-        .where({id}).first()
+        .where({id})
+        .first()
+}
+
+function remove(id) {
+    return db("schemes")
+        .where({id})
+        .del()
+}
+
+function findSteps(id) {
+    return db("steps")
+        .join("schemes", "schemes.id", "steps.scheme_id")
+        .select("steps.id", "schemes.scheme_name", "steps.step_number", "steps.instructions")
+        .where("scheme_id", id)
 }
